6eb11be7cbe217b03a23e7b0cc930e47ae4998e7
Furubayashiyuuya <cbyf1210195@gn.iwasaki.ac.jp>
2023-07-24 16:08:40 +0900

    15:53

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
+
+//require('./Home.css');
+function Home() {
+  const React = require('react');  
+  const styles = {
+    body: {
 <<<<<<< HEAD
-/////
-//require('./Home.css');
-function Home() {
-  const React = require('react');  
-  const styles = {
-    body: {
       backgroundColor: 'rgb(228, 112, 29)',
     },
     main: {
@@ -26,55 +26,20 @@ function Home() {
       margin: 'auto',
       marginTop: '15px',
       marginBottom: '15px',
-    },
-  };
-
-
-  return (
-
-    React.createElement('div', {style:styles.body},
-    React.createElement('main', {style:styles.main},
-      React.createElement('h1', { className: 'title' }, 'タイピングゲーム'),
-      React.createElement('button',{ className: 'gamebutton' }, React.createElement('a', { href: '/game-app/ITtyping' }, 'IT用語')),
-      React.createElement('button', { className: 'gamebutton' }, React.createElement('a', {href: '/game-app/Styping' }, '流行語・受賞語(昭和)')),
-      React.createElement('button',{ className: 'gamebutton' }, React.createElement('a', { href: '/game-app/Htyping' }, '流行語・受賞語(平成元年~平成20年)')),
-      React.createElement('button',{ className: 'gamebutton' }, React.createElement('a', { href: '/game-app/HRtyping' }, '流行語・受賞語(平成21年~)')),
-      React.createElement('p', null, '制限時間は5分(300秒)です。'),
-      React.createElement('p', null, '「○○ガチャ」や「炎上○○」の様な○○を含む流行語は除外してあります。<br />「Enter」を押すと問題が変わり、正解ならばスコアが加算されます。')
-    )
-  )
-  );
-}
-
-module.exports = Home;
 =======
-
-//require('./Home.css');
-function Home() {
-  const React = require('react');  
-  const styles = {
-    body: {
-      backgroundColor: 'rgb(228, 112, 29)',
+      backgroundColor: 'rgb(177, 177, 177)',
+      color: '#fff',
     },
-    main: {
-      backgroundColor: 'lightgray',
-      marginTop: '10%',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      border: 'solid',
+    div: {
+      marginLeft: '20%',
+      width: '60%',
+      border: '2px solid',
+      borderBottom: 'none',
+      fontSize: 'large',
     },
-    title: {
-      fontSize: '50px',
-      marginBottom: '20px',
-    },
-    gameButton: {
-      fontSize: '30px',
-      display: 'flex',
-      margin: 'auto',
-      marginTop: '15px',
-      marginBottom: '15px',
+    h1: {
+      textAlign: 'center',
+>>>>>>> e3da7a679853d56d520b55149aaa71865ce7abe1
     },
   };
 
@@ -82,6 +47,7 @@ function Home() {
   return (
 
     React.createElement('div', {style:styles.body},
+<<<<<<< HEAD
     React.createElement('main', {style:styles.main},
       React.createElement('h1', { className: 'title' }, 'タイピングゲーム'),
       React.createElement('button',{ className: 'gamebutton' }, React.createElement('a', { href: '/game-app/ITtyping' }, 'IT用語')),
@@ -92,8 +58,35 @@ function Home() {
       React.createElement('p', null, '「○○ガチャ」や「炎上○○」の様な○○を含む流行語は除外してあります。<br />「Enter」を押すと問題が変わり、正解ならばスコアが加算されます。')
     )
   )
+=======
+    React.createElement(
+      'div',{style:styles.div},
+      React.createElement('h1', null, '自己紹介'),
+      React.createElement('div',null,
+        React.createElement('ul',null,
+          React.createElement('li',null,'名前',
+            React.createElement('p', null, 'text')),
+          React.createElement('li',null,'趣味',
+            React.createElement('p', null, 'ゲーム(スマートフォンゲーム)')),
+          React.createElement('li',null,'スキル',
+            React.createElement('ul',null,
+              React.createElement('li', null, '.net →SQLのデータ操作アプリ、日記帳を作れる程度'),
+              React.createElement('li', null, 'PHP →ブラウザ上でSQLのデータの並び替え等の物が作れる程度'),
+              React.createElement('li', null, 'Java →基本的な文法(派生、継承)までは理解している'),
+              React.createElement('li', null, 'HTML/CSS →簡単なWebページなら作れる程度'),
+              React.createElement('li', null, 'JavaScrpit →Obnizを使った椅子の利用時間が分かるシステムを作製'),
+              React.createElement('li', null, 'React → railwayで初めて取り組んだ'),
+              React.createElement('li', null, 'AWS →DB,Webサーバの立ち上げ、ロードバランサーの設定経験あり')
+            )
+          )
+        ),
+        React.createElement('button',{ className: 'gamebutton' }, React.createElement('a', { href: '/ssrmy-app/My' }, 'ガクチカ・自己PR')),
+      )
+    ))      
+    
+  
+>>>>>>> e3da7a679853d56d520b55149aaa71865ce7abe1
   );
 }
 
 module.exports = Home;
->>>>>>> 1adeb664440dce7d4f5784b3ba079c5ab77868da
